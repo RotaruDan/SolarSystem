e86b32488810246d5348c2af4d21957a861ee3c8
RotaruDan <drotaru@ucm.es>
2015-03-23 15:41:14 +0100

    Added Mesh object.

---
+++ freeglut project 3D/Mesh.cpp
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+// Dan Cristian, Rotaru
+//---------------------------------------------------------------------------
+#include "Mesh.h"
+
+	Mesh::Mesh(char* charname) {
+		model = glmReadOBJ(charname);
+	}
+
+	Mesh::Mesh(char* charname, GLfloat scale) {
+		model = glmReadOBJ(charname);
+		glmScale(model, scale);
+	}
+
+	Mesh::~Mesh(){
+		glmDelete(model);
+	}
+
+	void Mesh::draw(){		
+		glmDraw(model, GL_SMOOTH);
+	}
